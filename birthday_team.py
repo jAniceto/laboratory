cdb23c62abbca8b0cdf446154f26123ccfa70258
José Aniceto <jose.pedro.aniceto@gmail.com>
2018-03-24 18:28:15 +0000

    Creates and send mail notification

--- birthday_team.py
+++ birthday_team.py
@@ -1,19 +1,16 @@
 from random import randint
 from datetime import datetime
 
-
-MEMBERS = [['Zé Pedro', 'joseaniceto@ua.pt', '22-7'],
-           ['Simão', 'simaocardoso@ua.pt', '21-4'],
-           ['Marcelo', 'marcelomelo@ua.pt', '3-5'],
-           ['Ivo', 'ivoazenha@ua.pt', '3-13'],
-           ['Martinique', '@ua.pt', '3-13'],
-           ['Bruno', '@ua.pt', '3-13'],
-           ['Vitor', '@ua.pt', '3-13']]
+import group_info
+import notify
 
 
 def draw_team(team_size=2):
+    """
+    Draw group member for the birthday team.
+    """
     team = []
-    members_pool = MEMBERS
+    members_pool = group_info.birthdays()
 
     for i in range(team_size):
         draw = randint(0, len(members_pool)-1)
@@ -28,6 +25,10 @@ def draw_team(team_size=2):
 
 
 def birthday_overlap(date):
+    """
+    Checks for birthdays overlaps.
+    """
+
     team_month = datetime.now().month+1
     month = int(date.split('-')[-1])
 
@@ -38,16 +39,69 @@ def birthday_overlap(date):
 
 
 def birthdays_next_month():
+    """
+    Checks for upcoming birthays (following month).
+    """
+
     team_month = datetime.now().month+1
     bday_list = []
 
-    for member in MEMBERS:
+    for member in group_info.birthdays():
         if int(member[2].split('-')[-1]) == team_month:
             bday_list.append(member)
 
     return bday_list
 
 
-if __name__ == '__main__':
-    print(draw_team())
+def create_message(bday_team, bday_list):
+    """
+    Creates the html code for a message to be sent to the birthdays team containing team members and upcoming birthdays
+    info.
+    """
+
+    month_dict = {1: 'janeiro', 2: 'fevereiro', 3: 'março', 4: 'abril', 5: 'maio', 6: 'junho',
+                  7: 'julho', 8: 'agosto', 9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'}
+
+    team_month = datetime.now().month + 1
+
+    # Simple message
+    message = '''Olá\n\nFazes parte da equipa de aniversários para o mês de {}.
+    '''.format(month_dict[team_month])
+
+    # HTML message
+    team_string = ''
+    for team_member in bday_team:
+        team_string += '{} '.format(team_member[0])
+
+    bdays_string = ''
+    for bdays in bday_list:
+        bdays_string += '<li>Dia {} - {}</li>'.format(bdays[2].replace('-', '/'), bdays[0])
+
+    html_message = '''<p>Olá.</p>
+    <p>Fazes parte da equipa de aniversários para o mês de {}. Equipa: {}</p>
+    <p>Os aniversários em {} são:</p>
+    <ul>{}</ul><br>
+    <p>Zé Pedro<br><small>Esta mensagem é automática. Em caso de erro contactar joseaniceto@ua.pt.</small></p>
+        '''.format(month_dict[team_month], team_string, month_dict[team_month], bdays_string)
+
+    return message, html_message
+
+
+def main():
+    # Draw a birthday team at random
+    bday_team = draw_team()
+    print(bday_team)
+
+    # Get next month birthdays
+    next_bdays = birthdays_next_month()
     print(birthdays_next_month())
+
+    # If there are birthdays next month, create e-mail and send
+    if next_bdays:
+        msg, msg_html = create_message(bday_team, next_bdays)
+        subject = 'Lembrete equipa de aniversários'
+        # notify.html_mail(subject, msg, msg_html)
+
+
+if __name__ == '__main__':
+    main()
---
+++ notify.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import smtplib
+
+# Required for basic mails
+from email.message import EmailMessage
+
+# Required for HTML mails
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def mail(subject, message, sender='aniceto.scripts@gmail.com', receiver='havok_2004@hotmail.com'):
+
+    if sender == 'aniceto.scripts@gmail.com':
+        pwd = os.getenv('MAIL_PASS')
+    else:
+        print('Sender e-mail and password don\'t match')
+        sys.exit()
+
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender, pwd)
+    server.sendmail(sender, receiver, msg.as_string())
+    server.close()
+
+    print('\nMail sent!')
+
+
+def html_mail(subject, message, message_html, sender='aniceto.scripts@gmail.com', receiver='havok_2004@hotmail.com'):
+
+    if sender == 'aniceto.scripts@gmail.com':
+        pwd = 'anicetopython'
+    else:
+        print('Sender e-mail and password don\'t match')
+        sys.exit()
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(message, 'plain')
+    part2 = MIMEText(message_html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender, pwd)
+    server.sendmail(sender, receiver, msg.as_string())
+    server.close()
+
+    print('\nMail sent!')
