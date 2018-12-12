a35bbf479dc24eb72eb99e9075b27229613dc5a7
José Aniceto <jAniceto@users.noreply.github.com>
2018-12-12 17:48:19 +0000

    Small fixes

--- birthday-team/birthday_team.py
+++ birthday-team/birthday_team.py
@@ -11,6 +11,18 @@ logging.basicConfig(filename='log.txt', level=logging.DEBUG,
 
 GROUP_INFO = group_info.birthdays()
 
+
+def team_month():
+    """
+    Determines the relevant month (always the next month)
+    """
+    if datetime.now().month == 12:
+        month_number = 1
+    else:
+        month_number = datetime.now().month+1
+    return month_number
+
+
 def draw_team(team_size=2):
     """
     Draw group member for the birthday team.
@@ -35,10 +47,9 @@ def birthday_overlap(date):
     Checks for birthdays overlaps.
     """
 
-    team_month = datetime.now().month+1
     month = int(date.split('-')[-1])
 
-    if month == team_month:
+    if month == team_month():
         return True
     else:
         return False
@@ -49,11 +60,9 @@ def birthdays_next_month():
     Checks for upcoming birthays (following month).
     """
 
-    team_month = datetime.now().month+1
     bday_list = []
-
     for member in GROUP_INFO:
-        if int(member[2].split('-')[-1]) == team_month:
+        if int(member[2].split('-')[-1]) == team_month():
             bday_list.append(member)
 
     return bday_list
@@ -68,31 +77,33 @@ def create_message(bday_team, bday_list):
     month_dict = {1: 'janeiro', 2: 'fevereiro', 3: 'março', 4: 'abril', 5: 'maio', 6: 'junho',
                   7: 'julho', 8: 'agosto', 9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'}
 
-    team_month = datetime.now().month + 1
+    month_index = team_month()
 
     # Subject
-    subject = 'Equipa de aniversários para {}'.format(month_dict[team_month])
+    subject = 'Equipa de aniversários para {}'.format(month_dict[month_index])
 
     # Simple message
     message = '''Olá\n\nA equipa de aniversários para o mês de {} é:
-    '''.format(month_dict[team_month])
+    '''.format(month_dict[month_index])
 
     # HTML message
     team_string = ''
     for team_member in bday_team:
-        team_string += '{} '.format(team_member[0])
+        team_string += '<li>{}</li>'.format(team_member[0])
 
     bdays_string = ''
     for bdays in bday_list:
         bdays_string += '<li>Dia {} - {}</li>'.format(bdays[2].replace('-', '/'), bdays[0])
 
     html_message = '''<p>Olá.</p>
-    <p>A equipa de aniversários para o mês de {} é: {}</p>
+    <p>A equipa de aniversários para o mês de {} é:</p>
+    <ul>{}</ul><br>
+    <br>
     <p>Os aniversários em {} são:</p>
     <ul>{}</ul><br>
     <p>Este e-mail foi enviado para todos os membros do grupo, excepto os aniversariantes.</p>
     <p>Zé Pedro<br><small>Esta mensagem é automática. Em caso de erro contactar joseaniceto@ua.pt.</small></p>
-        '''.format(month_dict[team_month], team_string, month_dict[team_month], bdays_string)
+        '''.format(month_dict[month_index], team_string, month_dict[month_index], bdays_string)
 
     return subject, message, html_message
 
@@ -109,7 +120,7 @@ def main():
         logging.info('Birthday team: {}'.format(bday_team))
 
         # Create and send e-mail
-        members_pool = GROUP_INFO
+        members_pool = group_info.birthdays()
         mail_to = [member[1] for member in members_pool if member not in next_bdays]
         sub, msg, msg_html = create_message(bday_team, next_bdays)
         for mail_address in mail_to:
